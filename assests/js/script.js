bfa95e709ca05ebec21e53482b17e2f253f60994
Dark-Assualt <karanrjsgurjar@gmail.com>
2023-09-06 15:16:25 +0530

    slider changes

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,6 +9,10 @@ var swiper = new Swiper(".mySwiper", {
       delay: 3500,
       disableOnInteraction: false,
     },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
